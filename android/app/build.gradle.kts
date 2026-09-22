c4696665d5231940efecf82492acec0eb04f92be
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-09-22 17:07:50 -0300

    "proyecto avanzado"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.flutter_application_01"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
